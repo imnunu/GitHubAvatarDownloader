087420e059c238550c541976f0d18a7f0c3a3e8a
Ellen Chang <imnunu@gmail.com>
2017-03-14 03:37:35 +0000

    passed header

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+// var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -7,20 +8,36 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var GITHUB_USER = "imnunu";
   var GITHUB_TOKEN = "5d3b0a24f22cd651932d987240e5bf1a4bbd28c7";
 
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-//   request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
-//        .on('error', function (err) {                                   // Note 2
-//          throw err;
-//        })
-//        .on('response', function (response) {                           // Note 3
-//          console.log('Response Status Message: ', response.statusMessage, 'content-type', response.headers['content-type'], '\nDownload complete.');
-//        })
-//        .pipe(fs.createWriteStream('./future.jpg'));               // Note 4
+  var requestURL = { url:('https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors') , headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'}};
+  // console.log(requestURL);
 
+  request(requestURL, function (error, response, body) {
+
+    if (!error && response.statusCode == 200) {
+
+      var parsed = JSON.parse(body);
+      cb(error, parsed);
+        // console.log(body);
+
+        // let data = JSON.parse(body).data;
+
+        // if (data && data.length) {
+        // data.forEach(cb);
+        // } else {
+        // console.log(`Nothing found.`);
+        //   }
+    }
+    //Check for error
+    if(error){
+        return console.log('Error:', error);
+    }
+
+    //Check for right status code
+    if(response.statusCode !== 200){
+        return console.log('Invalid Status Code Returned:', response.statusCode);
+    }
+  });
 }
-
-
 
 
 getRepoContributors("nodejs", "node", function(err, result) {
@@ -29,5 +46,3 @@ getRepoContributors("nodejs", "node", function(err, result) {
 });
 
 
-
-// curl -u imnunu:5d3b0a24f22cd651932d987240e5bf1a4bbd28c7 -I https://api.github.com/users/lighthouse-labs
