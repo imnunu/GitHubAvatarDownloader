91b52f6172d81eb1d4e30f766396e6ddd873ed45
Ellen Chang <imnunu@gmail.com>
2017-03-14 19:24:54 +0000

    step 7

--- download_avatars.js
+++ download_avatars.js
@@ -1,46 +1,56 @@
-var request = require('request');
-// var fs = require('fs');
+const request = require('request');
+const fs = require('fs');
+const GITHUB_USER = "imnunu";
+const GITHUB_TOKEN = "5d3b0a24f22cd651932d987240e5bf1a4bbd28c7";
+
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var GITHUB_USER = "imnunu";
-  var GITHUB_TOKEN = "5d3b0a24f22cd651932d987240e5bf1a4bbd28c7";
-
-  var requestURL = { url:('https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors') , headers: {'User-Agent': 'GitHub Avatar Downloader - Student Project'}};
-  // console.log(requestURL);
-
-  request(requestURL, function (error, response, body) {
-
-    if (!error && response.statusCode == 200) {
-
-      let data = JSON.parse(body);
-
-      var result = data.map(function(value) {
-        console.log(value.avatar_url);
-      })
-    //   if (data && data.length) {
-    //   data.forEach(cb);
-    //   } else {
-    //     console.log(`No data found.`);
-    //     }
-    // }
-    //Check for error
-    if(error){
-        return console.log('Error:', error);
+  const options = {
+    url: 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'Lighthouse Labs student project'
     }
+  };
 
-    //Check for right status code
-    if(response.statusCode !== 200){
-        return console.log('Invalid Status Code Returned:', response.statusCode);
-    }
+  request(options, function (error, response, body) {
+
+    if(error) {
+      throw error;
+    } else if (response.statusCode == 200) {
+
+        cb (JSON.parse(response.body));
+      }
+  });
+}
+//==========================================================
+
+getRepoContributors("jquery", "jquery", function(result) {
+
+  result.forEach(function(avatar){
+    // console.log(avatar.avatar_url);
+    downloadImageByURL(avatar.avatar_url, avatar.login)
+  });
+
+
+  function downloadImageByURL(url, filePath) {
+
+    request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Message: ', response.statusMessage);
+       })
+       .pipe(fs.createWriteStream("./avatars/" + filePath + ".jpg"))
+
   }
 });
-}
 
-
-getRepoContributors("nodejs", "node", function(err, result) {
-  console.log("Errors:", err);
-  console.log('result:', result);
-});
+  // getRepoContributors("jquery", "jquery", function(err, result) {
+  //   console.log("Errors:", err);
+  //   console.log("Result:", result);
+  // });
